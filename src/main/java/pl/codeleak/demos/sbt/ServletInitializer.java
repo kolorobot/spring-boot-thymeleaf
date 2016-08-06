058edf1aad596b9520eb4231c4bbeef344181b8d
kolorobot <rafal.borowiec@gmail.com>
2016-08-06 15:33:53 +0200

    Spring Boot 1.4.0.RELEASE + Thymeleaf 3

--- src/main/java/pl/codeleak/demos/sbt/ServletInitializer.java
+++ src/main/java/pl/codeleak/demos/sbt/ServletInitializer.java
@@ -1,7 +1,7 @@
 package pl.codeleak.demos.sbt;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
     @Override
